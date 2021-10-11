ab914e5acdd1dfb844b11b74d04e4cf81887f0f5
Pamela Montanez <pmontanez2@oreillyauto.com>
2021-10-11 12:55:17 -0700

    add font awesome package, update contac form, add mobile menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="app">
-  <div id="main-nav" class="main-nav">
+  <div id="main-nav" v-if="this.windowWidth > 480" class="main-nav">
     <router-link to="/" @mouseover="hoverEffect">Home</router-link>
     <router-link to="/resume">Resume</router-link>
     <router-link to="/projects">Projects</router-link>
@@ -11,6 +11,18 @@
       <router-link to="/case-studies/sgf-wit-website">SGF WIT</router-link>
     </div>
     <router-link to="/contact">Contact</router-link>
+  </div>
+  <div v-show="this.windowWidth <= 480" class="main-nav">
+    <span class="mobile-web-id"><a href="/">pamelamontanez.com</a></span>
+    <font-awesome-icon v-if="!openMenu" @click="openMenu = !openMenu" icon="bars" class="fa-xl mobile-menu" />
+    <font-awesome-icon v-if="openMenu" @click="openMenu = !openMenu" icon="times" class="fa-xl mobile-menu" />
+  </div>
+  <div v-show="openMenu" class="mobile-nav" ref="mobileMenu">
+    <router-link to="/" @click="openMenu = false">Home</router-link>
+    <router-link to="/resume" @click="openMenu = false">Resume</router-link>
+    <router-link to="/projects" @click="openMenu = false">Projects</router-link>
+    <router-link to="/case-studies" @click="openMenu = false">Case Studies</router-link>
+    <router-link to="/contact" @click="openMenu = false">Contact</router-link>
   </div>
   <router-view />
   <div class="content"></div>
@@ -30,23 +42,56 @@ export default {
   },
   data() {
     return {
-      caseStudiesOpen: false
+      caseStudiesOpen: false,
+      openMenu: false,
+      windowWidth: window.innerWidth
     }
   },
   mounted() {
-    window.addEventListener('click', e => {
+    window.addEventListener('click', this.clickHandler)
+    window.addEventListener('resize', this.onResize)
+  },
+  methods: {
+    onResize() {
+        this.windowWidth = window.innerWidth
+    },
+    clickHandler(e) {
       if (e.target === this.$refs.caseStudies && !this.caseStudiesOpen) {
         this.caseStudiesOpen = true
       } else {
         this.caseStudiesOpen = false
       }
-    })
+    },
+    toggleMenu(e) {
+      if (!this.$refs.mobileMenu.contains(e.target)) {
+        this.openMenu = false
+      }
+    }
+  },
+  watch: {
+    openMenu(val) {
+      if (val) {
+        setTimeout(() => {
+          window.addEventListener('click', this.toggleMenu)
+          console.log('added')
+        }, 500)
+        window.removeEventListener('click', this.toggleMenu)
+        console.log('removed')
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss">
 @import '@/styles/styles.scss';
+
+.mobile-web-id {
+  text-align: left;
+  width: 100%;
+  padding: 6px 0;
+  height: 22px;
+}
 
 a {
   cursor: pointer;
--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -9,23 +9,24 @@
                 <img src="@/assets/airplane.gif" />
             </div>
             <div class="text-block-contact">
-                <h2 class="photo-title">Send me a note.</h2>
-                <h2>Contact Form</h2>
+                <!-- <h2 class="photo-title">Send me a note.</h2> -->
                 <div class="contact-form">
-                    <form>
-                        <label for="name">Name: </label>
-                        <input type="text" id="name" name="firstname">
-
-                        <label for="email">Email: </label>
-                        <input type="text" id="email" name="email">
-
-                        <label for="message">Message: </label>
-                        <textarea id="message" name="message"></textarea>
-                        <div class="g-recaptcha" data-sitekey="6LfFwMUUAAAAAEEFtG77rx4g5Ur2wLabqthEYTaj"></div>
-                        <button type="button" @click="location.href='mailto:pamrmont@gmail.com';">Send</button>
-                    </form>
+                    <iframe id="iframe" src="https://docs.google.com/forms/d/e/1FAIpQLSf3AninhYbF6wZB9TpFu2XHqzmMt77feIH8K11owGYxjuB7Og/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style lang="scss">
+iframe {
+    width: 100%;
+    min-height: 700px;
+}
+
+@media only screen and (max-width: 450px) {
+    iframe {
+    min-height: 690px;
+    }
+}
+</style>
