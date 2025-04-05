a94a6b350e303c56c86ff18185753562743da2a7
fancypams <pamrmont@gmail.com>
2025-04-04 22:55:30 -0700

    add bookshelf using openlibrary api

--- src/components/Bookshelf.vue
+++ src/components/Bookshelf.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-      
+  <div class="section-title">Books</div>
+  <div class="section-subtitle">A short list of my latest mentionable reads shown utilizing the <a href="https://openlibrary.org/" target="_blank">open library</a> api</div>
+  <div class="book-shelf">
+    <div class="category" v-for="book in finished" :key="book">
+      <span class="book-cover"><img :src="book.cover" /></span>
+      <span class="book-title">{{ book.title }}</span>
+      <span class="book-author">{{ book.author[0] }}</span>
     </div>
+  </div>
 </template>
 
 <script>
@@ -13,13 +19,26 @@ export default {
   },
   data() {
       return {
-
+        finished: [],
+        reading: [],
       }
   },
   mounted() {
     axios
-      .get('https://api.weatherapi.com/v1/current.json?key=b0c1c95d4c7942faab210856250404&q=98126&aqi=no')
-      .then(response => (this.weather = response.data.current.condition.text))
+      .get('https://openlibrary.org/people/pamelamontanez/books/already-read.json')
+      .then(response => {
+        let entries = response.data.reading_log_entries
+        console.info(response.data)
+        for (let i = 0; i < entries.length; i++) {
+          let obj = {
+            title: entries[i].work.title,
+            author: entries[i].work.author_names,
+            olid: entries[i].work.edition_key[0],
+            cover: 'https://covers.openlibrary.org/b/olid/' + entries[i].work.cover_edition_key + '-M.jpg'
+          }
+          this.finished.push(obj)
+        }
+      })
   },
   methods: {
 
@@ -30,12 +49,75 @@ export default {
 <style lang="scss">
 @import '@/styles/styles.scss';
 
+.section-title {
+  font-family: $heading;
+  font-size: 128px;
+  text-transform: uppercase;
+  font-weight: 100;
+  border-bottom: double 3px $black;
+  text-align: left;
+  width: 100%;
+}
+.section-subtitle {
+  font-size: 14px;
+  margin-bottom: 50px;
+  margin-top: -35px;
+}
+.book-shelf {
+  display: flex;
+  flex-flow: row;
+  gap: 20px;
+  margin: 20px 0;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: left;
+  .category {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+  }
+  .book-title {
+    font-weight: 300;
+    font-family: $sans-serif;
+    width: 130px;
+    height: 30px;
+    text-align: center;
+  }
+  .book-author {
+    font-style: italic;
+    max-width: 300px;
+    text-align: center;
+    font-size: 14px;
+  }
+  .book-cover img{
+    height: 175px;
+
+  }
+}
+a {
+  font-size: 12px;
+}
 
 @media only screen and (max-width: 800px) {
 
 }
 
 @media only screen and (max-width: 450px) {
-  
+  .section-title {
+    font-size: 54px;
+    line-height: 45px;
+    padding-bottom: 15px;
+    text-align: center;
+  }
+  .section-subtitle {
+    margin-bottom: 20px;
+    margin-top: 20px;
+    text-align: center;
+  }
+  .book-shelf {
+    justify-content: center;
+  }
 }
 </style>
