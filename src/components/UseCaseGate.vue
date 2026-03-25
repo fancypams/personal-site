05e687b991dde636801f391451b8a24a0a628a78
fancypams <pamrmont@gmail.com>
2026-03-25 10:35:32 -0700

    copy updates

--- src/components/UseCaseGate.vue
+++ src/components/UseCaseGate.vue
@@ -32,7 +32,7 @@ onMounted(() => {
 })
 
 function attempt() {
-  if (password.value === "viewusecase") {
+  if (password.value === "pft") {
     sessionStorage.setItem('uc-unlocked', '1')
     unlocked.value = true
     error.value = false
